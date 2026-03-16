979de5e54da3a752fbb0a6ad5fb8f3b3bde32815
jpagcal <84163162+jpagcal@users.noreply.github.com>
2026-03-16 01:20:29 +0000

    Deploying to gh-pages from @ jpagcal/net-plus-plus@37656a40a02c280ed4d976777b6b327c319332eb 🚀

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['recv_5fasync_0',['recv_async',['../classtcp_1_1Connection.html#a6552f9b7b0d23e9444eef0ed5e90e04b',1,'tcp::Connection::recv_async(std::vector&lt; std::byte &gt; buf, std::function&lt; void()&gt; callback)'],['../classtcp_1_1Connection.html#aed6b6f06c8a962a484542bf4d31f16fb',1,'tcp::Connection::recv_async(std::vector&lt; std::byte &gt; buf)']]],
-  ['resolver_1',['Resolver',['../classconn__resolver_1_1Resolver.html#a542e0f3cf0eeba59494329806cf81073',1,'conn_resolver::Resolver']]],
-  ['results_2',['results',['../classconn__resolver_1_1Resolver.html#ae01b93fb698e2d516115b9e3ef605ef7',1,'conn_resolver::Resolver']]]
+  ['recv_5fasync_0',['recv_async',['../classtcp_1_1Connection.html#a6552f9b7b0d23e9444eef0ed5e90e04b',1,'tcp::Connection']]],
+  ['recv_5fsync_1',['recv_sync',['../classtcp_1_1Connection.html#a73e7ce6aa439176ea61994bc05341c4d',1,'tcp::Connection']]],
+  ['resolver_2',['Resolver',['../classconn__resolver_1_1Resolver.html#a542e0f3cf0eeba59494329806cf81073',1,'conn_resolver::Resolver']]],
+  ['results_3',['results',['../classconn__resolver_1_1Resolver.html#ae01b93fb698e2d516115b9e3ef605ef7',1,'conn_resolver::Resolver']]]
 ];
